4bfb70482aae6577972cda3cba787e976bedfc04
xidedix <xidedix@users.noreply.github.com>
2018-05-08 18:14:11 +0200

    refactor(demo): sync with CoreUI-React template

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,8 @@
+import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
-import './polyfill'
 
 ReactDOM.render(<App />, document.querySelector('#demo'))
 // ReactDOM.render(<App />, document.getElementById('root'));
