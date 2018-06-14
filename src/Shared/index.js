942a989f50ff4a663463523982df709660b56888
Kumar Gaurav <krgaurav1992@gmail.com>
2018-06-14 16:35:24 +0530

    Fixed missing AppLayout module in '.Shared' error

--- src/Shared/index.js
+++ src/Shared/index.js
@@ -1,3 +1,5 @@
 import { sidebarCssClasses, asideMenuCssClasses } from './classes';
+import _AppLayout from './layout/layout';
 
 export { sidebarCssClasses, asideMenuCssClasses };
+export default _AppLayout;
