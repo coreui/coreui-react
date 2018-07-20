df2c8cda8befaba469c0d403577e37114f81afc9
xidedix <xidedix@users.noreply.github.com>
2018-07-20 19:14:39 +0200

    test: adjust SidebarToggler.test

--- tests/SidebarToggler.test.js
+++ tests/SidebarToggler.test.js
@@ -13,7 +13,7 @@ configure({ adapter: new Adapter() });
 describe('AppSidebarToggler', () => {
   it('renders button with class="navbar-toggler"', () => {
     expect(render(<AppSidebarToggler className="d-lg-none" display="md" mobile />))
-    .toContain('<button type="button" class="d-lg-none navbar-toggler"><span class="navbar-toggler-icon"></span></button>')
+    .toContain('<button type="button" class="d-lg-none navbar-toggler" data-sidebar-toggler="true"><span class="navbar-toggler-icon"></span></button>')
   })
   it('should call sidebarToggle', () => {
     let component = mount(<AppSidebarToggler />);
