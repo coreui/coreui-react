d7ecc4f7e6b6fbc9a595bf0462938485a39dedef
xidedix <xidedix@users.noreply.github.com>
2018-11-06 16:45:38 +0100

    chore(demo): update style.css to `@coreui/coreui v2.1.0`, navLink `disabled` example

--- demo/src/_nav.js
+++ demo/src/_nav.js
@@ -8,6 +8,27 @@ export default {
         variant: 'info',
         text: 'NEW'
       }
-    }
+    },
+    {
+      name: 'Disabled',
+      url: '/dashboard',
+      icon: 'cui-ban icons',
+      attributes: { disabled: true },
+    },
+    {
+      name: 'Download CoreUI',
+      url: 'https://coreui.io/react/',
+      icon: 'cui-cloud-download icons',
+      class: 'mt-auto',
+      variant: 'success',
+      attributes: { target: '_blank', rel: "noopener" },
+    },
+    {
+      name: 'Try CoreUI PRO',
+      url: 'https://coreui.io/pro/react/',
+      icon: 'cui-layers icons',
+      variant: 'danger',
+      attributes: { target: '_blank', rel: "noopener" },
+    },
   ]
 };
