1956af30ac4e87e39213710e68b6c3bd7de9e5cd
Wojciech Tubek <wojciech.tubek@gmail.com>
2019-02-27 14:26:47 +0100

    removed force variable

--- src/Shared/toggle-classes.js
+++ src/Shared/toggle-classes.js
@@ -1,10 +1,10 @@
-export default function toggleClasses (toggleClass, classList, force) {
+export default function toggleClasses (toggleClass, classList) {
   const level = classList.indexOf(toggleClass)
   const removeClassList = classList.slice(0, level)
   removeClassList.map((className) => document.body.classList.remove(className))
   if (document.body.classList.contains(toggleClass)) {
-    document.body.classList.remove(toggleClass, force);
+    document.body.classList.remove(toggleClass);
   } else {
-    document.body.classList.add(toggleClass, force);
+    document.body.classList.add(toggleClass);
   }
 }
