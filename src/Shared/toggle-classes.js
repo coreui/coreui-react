05d4a8e5dae12c20295bf7047345a027e14936f1
xidedix <xidedix@users.noreply.github.com>
2019-03-05 17:43:07 +0100

    refactor(toggle-classes): keep `force` for the toggle method

--- src/Shared/toggle-classes.js
+++ src/Shared/toggle-classes.js
@@ -1,10 +1,13 @@
-export default function toggleClasses (toggleClass, classList) {
+export default function toggleClasses (toggleClass, classList, force) {
   const level = classList.indexOf(toggleClass)
   const removeClassList = classList.slice(0, level)
   removeClassList.map((className) => document.body.classList.remove(className))
-  if (document.body.classList.contains(toggleClass)) {
+  if (force === true) {
+    document.body.classList.add(toggleClass);
+  } else if (force === false) {
     document.body.classList.remove(toggleClass);
   } else {
-    document.body.classList.add(toggleClass);
+    document.body.classList.toggle(toggleClass);
   }
+  return document.body.classList.contains(toggleClass);
 }
