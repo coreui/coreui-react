cd135fe5e0f316e0ed7de4f29331a71af4a4e6a1
xidedix <xidedix@users.noreply.github.com>
2020-02-14 21:13:19 +0100

    chore: dependencies update and config refactor

--- nwb.config.js
+++ nwb.config.js
@@ -5,7 +5,8 @@ module.exports = {
     umd: {
       global: 'CoreUIReact',
       externals: {
-        react: 'React'
+        react: 'React',
+        'react-router': 'ReactRouter'
       }
     }
   }
