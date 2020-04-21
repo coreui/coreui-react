e0f9c0687f4f85d0ada02ffbf09c4b5bb0fea292
woothu <32914662+woothu@users.noreply.github.com>
2020-04-21 17:57:29 +0200

    fix: CCard: delete 'custom' and corelating props

--- src/CCard.js
+++ src/CCard.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {tagPropType, mapToCssModules} from './Shared/helper.js';
-import CCardBody from './CCardBody';
-import CCardHeader from './CCardHeader';
-import CCardFooter from './CCardFooter';
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { tagPropType, mapToCssModules } from './Shared/helper.js'
 
 //component - CoreUI / CCard
 
-const CCard = props=>{
+const CCard = props => {
 
   const {
     tag: Tag,
-    children,
     className,
     cssModule,
-    custom,
     //
     innerRef,
     color,
     textColor,
     borderColor,
-    accentColor,
-    headerSlot,
-    footerSlot,
     align,
-    bodyClassName,
-    bodyProps,
+    accentColor,
     ...attributes
-  } = props;
+  } = props
 
   //render
 
@@ -40,66 +31,37 @@ const CCard = props=>{
     color ? `bg-${color}` : false,
     borderColor ? `border-${borderColor}` : false,
     accentColor ? `card-accent-${accentColor}` : false,
-  ), cssModule);
+  ), cssModule)
 
-  if (!custom)
-    return (
-      <Tag {...attributes} className={classes} ref={innerRef}>
-        {headerSlot?(
-        <CCardHeader>
-          {headerSlot}
-        </CCardHeader>
-        ): ''}
-        <CCardBody {...bodyProps} className={bodyClassName}>
-          {children}
-        </CCardBody>
-        {footerSlot?(
-        <CCardFooter>
-          {footerSlot}
-        </CCardFooter>
-        ): ''}
-      </Tag>
-    );
 
   return (
-    <Tag {...attributes} className={classes} ref={innerRef}>
-      {children}
-    </Tag>
-  );
-
+    <Tag 
+      className={classes} 
+      {...attributes} 
+      ref={innerRef}
+    />
+  )
 }
 
-CCard.sharedPropTypes = {
-  align: PropTypes.string,
+export const sharedPropTypes = {
+  align: PropTypes.oneOf(['', 'left', 'center', 'right']),
   color: PropTypes.string,
   borderColor: PropTypes.string,
-  textColor: PropTypes.string,
-  accentColor: PropTypes.string,
-  bodyClassName: PropTypes.string,
-  bodyProps: PropTypes.object
+  textColor: PropTypes.string
 }
 
 CCard.propTypes = {
-  ...CCard.sharedPropTypes,
   tag: tagPropType,
   className: PropTypes.string,
   cssModule: PropTypes.object,
-  custom: PropTypes.bool,
   //
   innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.string]),
-  align: PropTypes.string,
-  color: PropTypes.string,
-  borderColor: PropTypes.string,
-  textColor: PropTypes.string,
   accentColor: PropTypes.string,
-  bodyClassName: PropTypes.string,
-  bodyProps: PropTypes.object,
-  headerSlot: PropTypes.node,
-  footerSlot: PropTypes.node
+  ...sharedPropTypes
 }
 
 CCard.defaultProps = {
   tag: 'div'
 }
 
-export default CCard;
+export default CCard
