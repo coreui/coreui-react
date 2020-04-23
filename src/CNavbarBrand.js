ce4fdeb2ebbf90d9b36c16535800367927769c44
woothu <32914662+woothu@users.noreply.github.com>
2020-04-23 10:49:29 +0200

    fix: rewrite Brand components - make Brands simple wrappers

--- src/CNavbarBrand.js
+++ src/CNavbarBrand.js
@@ -1,88 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {tagPropType} from './Shared/helper.js';
+import React from 'react'
+import CBrand from './CBrand'
 
 //component - CoreUI / CNavbarBrand
 
-const CNavbarBrand = props=>{
-
-  const {
-    tag: Tag,
-    className,
-    children,
-    //
-    innerRef,
-    ...attributes
-  } = props;
-
-  const imgSrc = brand=>{
-    return brand.src ? brand.src : '';
-  }
-  const imgWidth = brand=>{
-    return brand.width ? brand.width : 'auto';
-  }
-  const imgHeight = brand=>{
-    return brand.height ? brand.height : 'auto';
-  }
-  const imgAlt = brand=>{
-    return brand.alt ? brand.alt : '';
-  }
-  const navbarBrandImg = (props, classBrand, key)=>{
-    return (
-      <img
-          src={imgSrc(props)}
-          width={imgWidth(props)}
-          height={imgHeight(props)}
-          alt={imgAlt(props)}
-          className={classBrand}
-          key={key.toString()}
-      />
-    );
-  }
-
-  //render
-
-  const classes = classNames(
-    className,
-    'navbar-brand'
-  );
-
-  const img = [];
-  if (props.brand) {
-    const classBrand = 'navbar-brand';//navbar-brand
-    img.push(navbarBrandImg(props.brand, classBrand, img.length + 1));
-  }
-  if (props.full) {
-    const classBrand = 'navbar-brand-full';
-    img.push(navbarBrandImg(props.full, classBrand, img.length + 1));
-  }
-  if (props.minimized) {
-    const classBrand = 'navbar-brand-minimized';
-    img.push(navbarBrandImg(props.minimized, classBrand, img.length + 1));
-  }
-
+const CNavbarBrand = props => {
   return (
-    <Tag {...attributes} className={classes} ref={innerRef}>
-      {children || img}
-    </Tag>
-  );
-
+    <CBrand {...props} className={['navbar-brand', props.className]}/>
+  )
 }
 
-CNavbarBrand.propTypes = {
-  tag: tagPropType,
-  className: PropTypes.string,
-  children: PropTypes.node,
-  //
-  innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.string]),
-  brand: PropTypes.any,
-  full: PropTypes.any,
-  minimized: PropTypes.any
-};
-
-CNavbarBrand.defaultProps = {
-  tag: 'div'
-};
-
-export default CNavbarBrand;
+export default CNavbarBrand
--- src/CSidebarBrand.js
+++ src/CSidebarBrand.js
@@ -1,122 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import CLink from './CLink';
+import React from 'react'
+import CBrand from './CBrand'
 
 //component - CoreUI / CSidebarBrand
 
-const CSidebarBrand = props=>{
-
-  let {
-    tag: Tag,
-    children,
-    className,
-    custom,
-    //
-    innerRef,
-    img,
-    imgFull,
-    imgMinimized,
-    wrappedInLink,
-    linkClassName,
-    linkProps,
-    ...attributes
-  } = props;
-
-  const imgSrc = brand=>{
-    return brand.src ? brand.src : '';
-  }
-  const imgWidth = brand=>{
-    return brand.width ? brand.width : 'auto';
-  }
-  const imgHeight = brand=>{
-    return brand.height ? brand.height : 'auto';
-  }
-  const imgAlt = brand=>{
-    return brand.alt ? brand.alt : '';
-  }
-  const navbarBrandImg = (props, classBrand, key)=>{
-    /*
-    const {
-      src,
-      width,
-      alt,
-      height,
-      ...attributes
-    } = props;
-    */
-    return (
-      <img
-          src={imgSrc(props)}
-          width={imgWidth(props)}
-          height={imgHeight(props)}
-          alt={imgAlt(props)}
-          className={classBrand}
-          {...props}
-          key={key.toString()}
-      />
-    );
-  }
-
-  // render
-
-  const classes = classNames(
-    className,
-    'c-sidebar-brand'
-  );
-
-  if (custom){
-    return (
-      <Tag className={classes} {...attributes} ref={innerRef}>
-        {children}
-      </Tag>
-    );
-  }
-
-  const classesLink = classNames(
-    linkClassName,
-    'c-sidebar-brand'
-  );
-
-  const imgChildren = [];
-  if (imgFull) {
-    imgChildren.push(navbarBrandImg(imgFull||img, 'c-sidebar-brand-full', 1));
-  }
-  if (imgMinimized) {
-    imgChildren.push(navbarBrandImg(imgMinimized||img, 'c-sidebar-brand-minimized', imgChildren.length + 1));
-  }
-
+const CSidebarBrand = props => {
   return (
-    <Tag {...attributes} className={classes} ref={innerRef}>
-      {
-        wrappedInLink ?
-          <CLink className={classesLink} {...linkProps} >
-            {imgChildren}
-          </CLink>
-        :imgChildren
-      }
-    </Tag>
-  );
-
+    <CBrand {...props} className={['c-sidebar-brand', props.className]}/>
+  )
 }
 
-CSidebarBrand.propTypes = {
-  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-  children: PropTypes.node,
-  className: PropTypes.string,
-  custom: PropTypes.bool,
-  //
-  innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.string]),
-  img: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  imgFull: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  imgMinimized: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  linkClassName: PropTypes.string,
-  linkProps: PropTypes.object,
-  wrappedInLink: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
-};
-
-CSidebarBrand.defaultProps = {
-  tag: 'div'
-};
-
-export default CSidebarBrand;
+export default CSidebarBrand
