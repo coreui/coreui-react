f5f4dd16bb86b3c35f5cd22b8a62afd465b0c7a5
woothu <32914662+woothu@users.noreply.github.com>
2020-04-24 15:24:09 +0200

    fix: CNav: delete 'horizontal' prop, clear component

--- src/CNav.js
+++ src/CNav.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {tagPropType, mapToCssModules} from './Shared/helper.js';
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { tagPropType, mapToCssModules } from './Shared/helper.js'
 
 //component - CoreUI / CNav
 
-const CNav = props=>{
+const CNav = props => {
 
   const {
     tag: Tag,
@@ -15,41 +15,31 @@ const CNav = props=>{
     innerRef,
     variant,
     vertical,
-    horizontal,
     justified,
     fill,
     inCard,
     ...attributes
-  } = props;
+  } = props
 
-  const getVerticalClass = vertical=>{
-    if (vertical === false) {
-      return false;
-    } else if (vertical === true || vertical === 'xs') {
-      return 'flex-column';
-    }
-    return `flex-${vertical}-column`;
-  };
+  const verticalClass = `flex${vertical === true ? '' : `-${vertical}`}-column`
 
   //render
 
   const classes = mapToCssModules(classNames(
-    className,
     'nav',
-    horizontal ? `justify-content-${horizontal}` : false,
-    variant=='tabs' ? 'nav-tabs' : variant=='pills' ? 'nav-pills' : null,
-    getVerticalClass(vertical),
+    vertical && verticalClass,
     {
-      'card-header-tabs': inCard && variant=='tabs',
-      'card-header-pills': inCard && variant=='pills',
+      [`nav-${variant}`]: variant,
       'nav-justified': justified,
       'nav-fill': fill,
-    }
-  ), cssModule);
+      [`card-header-${variant}`]: inCard && variant
+    },
+    className
+  ), cssModule)
 
   return (
-    <Tag {...attributes} className={classes} ref={innerRef} />
-  );
+    <Tag className={classes} {...attributes} ref={innerRef} />
+  )
 
 }
 
@@ -61,15 +51,13 @@ CNav.propTypes = {
   innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.string]),
   variant: PropTypes.oneOf(['', 'tabs', 'pills']),
   vertical: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-  horizontal: PropTypes.string,
   justified: PropTypes.bool,
   fill: PropTypes.bool,
   inCard: PropTypes.bool
 };
 
 CNav.defaultProps = {
-  tag: 'ul',
-  vertical: false,
+  tag: 'ul'
 };
 
-export default CNav;
+export default CNav
