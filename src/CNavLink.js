a4634fbac11df253fe7af8c7a437592834057f72
woothu <32914662+woothu@users.noreply.github.com>
2020-04-24 15:28:10 +0200

    refactor: CNavLink: concise component

--- src/CNavLink.js
+++ src/CNavLink.js
@@ -1,44 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {mapToCssModules} from './Shared/helper.js';
-import CLink from './CLink';
+import React from 'react'
+import CLink from './CLink'
 
-//component - CoreUI / CNavLink
-
-const CNavLink = props=>{
-
-  const {
-    className,
-    cssModule,
-    //
-    ...attributes
-  } = props;
-
-  //render
-
-  const classes = mapToCssModules(classNames(
-    className,
-    'nav-link',
-  ), cssModule);
-
+const CNavLink = props => {
   return (
-    <CLink {...attributes} className={classes} />
-  );
-
+    <CLink {...props} className={['nav-link', props.className]}/>
+  )
 }
 
-CNavLink.propTypes = {
-  className: PropTypes.string,
-  cssModule: PropTypes.object,
-  //
-  innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func, PropTypes.string]),
-  onClick: PropTypes.func,
-  href: PropTypes.string,
-  to: PropTypes.string
-};
-
-CNavLink.defaultProps = {
-};
-
-export default CNavLink;
+export default CNavLink
