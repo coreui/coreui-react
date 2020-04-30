ef444f0166b395fe33217cc95eced48b5a28cb6b
woothu <32914662+woothu@users.noreply.github.com>
2020-04-30 13:28:18 +0200

    chore: clean components

--- src/CBrand.js
+++ src/CBrand.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import { tagPropType, mapToCssModules } from './Shared/helper.js';
+import { tagPropType, mapToCssModules } from './Shared/helper.js'
 import CLink from './CLink'
 
 //component - CoreUI / CBrand
@@ -23,7 +23,7 @@ const CBrand = props => {
   const Tag = attributes.to || attributes.href ? CLink : tag
   const ref = { [`${typeof Tag === 'string' ? 'ref': 'innerRef'}`]: innerRef}
   return (
-    <Tag {...attributes} className={classes} {...ref} />
+    <Tag className={classes} {...attributes} {...ref} />
   )
 }
 
