e294b4b6df619e75496676a41d3aeceb91e2cf42
woothu <32914662+woothu@users.noreply.github.com>
2020-05-07 12:12:18 +0200

    feat: CCreateElement: add posibility of pasing component tag as string

--- src/CCreateElement.js
+++ src/CCreateElement.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 
-const renderItem = (item, i) => {
-  const { _tag: Tag, _children, ...rest } = item
-  const children = _children ? _children.map((child, i) => {
-    return typeof child === 'object' ? renderItem(child, i) : child
-  }) : ''
-  return <Tag key={Tag + i} {...rest}>{children}</Tag>
-}
-
 //component - CoreUI / CCreateElement
-const CCreateElement = props => {
+const CCreateElement = ({ items, components = {} }) => {
+  const renderItem = (item, i) => {
+    const { _tag, _children, ...rest } = item
+    const Tag = components[_tag] || _tag
+    const children = _children ? _children.map((child, i) => {
+      return typeof child === 'object' ? renderItem(child, i) : child
+    }) : ''
+    return <Tag key={Tag + i} {...rest}>{children}</Tag>
+  }
+
+  const generatedItems = useMemo(() => {
+    return items && items.map((item, i) => renderItem(item, i))
+  }, [JSON.stringify(items)])
+
   return (
     <React.Fragment>
-      {props.items && props.items.map((item, i) => renderItem(item, i))}
+      {generatedItems}
     </React.Fragment>
   )
 }
 
 CCreateElement.propTypes = {
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
+  components: PropTypes.object
 };
 
 export default CCreateElement
