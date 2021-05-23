1e7b5291efa6a1bbff91dd997ce5d87b051459ef
Łukasz Holeczek <lukasz@holeczek.pl>
2021-05-23 18:18:27 +0200

    fix: change coreui version

--- src/gatsby-theme-docz/index.js
+++ src/gatsby-theme-docz/index.js
@@ -2,7 +2,7 @@
 import { jsx } from 'theme-ui'
 import { theme, useConfig, ComponentsProvider } from 'docz'
 import { Styled, ThemeProvider } from 'theme-ui'
-import '@coreui/coreui-pro/scss/coreui.scss'
+import '@coreui/coreui/scss/coreui.scss'
 import './dosc.scss'
 
 import defaultTheme from '~theme'
