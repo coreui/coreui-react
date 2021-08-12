76d14a44c628fe2fee9e64f06633d6d47866a35a
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-12 16:24:12 +0200

    build: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 import pkg from './package.json'
 export default {
   input: 'src/index.ts',
