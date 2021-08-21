1875f6a4885c9a1fe839928de47bd966aebad6d9
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-22 01:20:01 +0200

    docs: update api generator

--- build/api.js
+++ build/api.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 'use strict'
 
 const docgen = require('react-docgen-typescript')
 const fs = require('fs').promises
 const path = require('path')
 const globby = require('globby')
+const pkg = require('../package.json')
 
-// These are the filetypes we only care about replacing the version
 const GLOB = ['src/components/**/*.tsx']
 const GLOBBY_OPTIONS = {
   cwd: path.join(__dirname, '..'),
@@ -22,20 +23,35 @@ const options = {
 async function createMdx(filename, name, props) {
   if (typeof props === 'undefined') return
 
-  // let content = `### ${name}\n\n| Prop name | Description | Type |\n| --- | --- | --- |\n`
-  let content = `| Prop name | Description | Type |\n| --- | --- | --- |\n`
+  let content = `| Property | Description | Type | Default |\n`
+  content += `| --- | --- | --- | --- |\n`
 
   for (const [key, value] of Object.entries(props).sort()) {
-    if (value.parent.fileName !== 'react/node_modules/@types/react/index.d.ts') {
-      content += `| ${value['name']} | ${value['description']} | ${value['type']['name']} |\n`
-      console.log(`${key}: ${value}`)
+    if (
+      value.parent.fileName !== 'react/node_modules/@types/react/index.d.ts' &&
+      !value['description'].includes('@ignore')
+    ) {
+      const name = value.name || ''
+      const description =
+        value.description.replaceAll('\n', '<br/>').replaceAll(' [docs]', '') || '-'
+      const type =
+        value.type ? value.type.name.includes('ReactElement')
+          ? 'ReactElement'
+          : value.type.name : ''
+      const defaultValue = value.defaultValue ? value.defaultValue.value : '-'
+
+      content += `| **${name}** | ${description} | \`${type}\` | ${defaultValue} |\n`
+      console.log(`${filename} - ${key}`)
     }
   }
 
-  await fs.writeFile(`api/${filename}.api.mdx`, content, { encoding: 'utf8' }).then(() => {
-    // Do whatever you want to do.
-    console.log('Done')
-  })
+  await fs
+    .writeFile(`docs/${pkg.config.version_short}/api/${filename}.api.mdx`, content, {
+      encoding: 'utf8',
+    })
+    .then(() => {
+      console.log(`File created: ${filename}.api.mdx`)
+    })
 }
 
 async function main() {
@@ -49,8 +65,6 @@ async function main() {
           const filename = path.basename(file, '.tsx')
           createMdx(filename, props[0].displayName, props[0].props)
         }
-        // const props = docgen.parse(file, options)
-        // if (props) console.log(props)
       }),
     )
   } catch (error) {
