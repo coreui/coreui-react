635de93640fff63c1cf82eaed0cc527a1a74dcfb
Łukasz Holeczek <lukasz@holeczek.pl>
2021-09-01 16:13:15 +0200

    fix(CImage): update interface

--- src/components/image/CImage.tsx
+++ src/components/image/CImage.tsx
@@ -1,8 +1,8 @@
-import React, { forwardRef, HTMLAttributes } from 'react'
+import React, { forwardRef, ImgHTMLAttributes } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-export interface CImageProps extends HTMLAttributes<HTMLImageElement> {
+export interface CImageProps extends ImgHTMLAttributes<HTMLOrSVGImageElement> {
   /**
    * Set the horizontal aligment.
    */
