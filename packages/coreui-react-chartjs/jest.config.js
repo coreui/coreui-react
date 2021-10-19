d6dc8a093ceeac974ebb6aac38f34a9b55770a4b
Łukasz Holeczek <lukasz@holeczek.pl>
2021-10-20 00:01:55 +0200

    tests: remove Jest config from package.json

---
+++ packages/coreui-react-chartjs/jest.config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2013-present, creativeLabs Lukasz Holeczek.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict'
+
+module.exports = {
+  preset: 'ts-jest',
+  setupFiles: ['jest-canvas-mock'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['dist/'],
+}
