64dad0458b14b4d953f6af4a225f789b2b341795
Łukasz Holeczek <lukasz@holeczek.pl>
2021-11-27 20:09:32 +0100

    refactor(CAccordion): Omit `horizontal` property

--- packages/coreui-react/src/components/accordion/CAccordionCollapse.tsx
+++ packages/coreui-react/src/components/accordion/CAccordionCollapse.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { CCollapse, CCollapseProps } from '../collapse/CCollapse'
 
-export const CAccordionCollapse = forwardRef<HTMLDivElement, CCollapseProps>(
+export const CAccordionCollapse = forwardRef<HTMLDivElement, Omit<CCollapseProps, 'horizontal'>>(
   ({ children, ...props }, ref) => {
     return (
       <CCollapse className="accordion-collapse" {...props} ref={ref}>
