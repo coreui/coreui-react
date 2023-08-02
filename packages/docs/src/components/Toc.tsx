9cdb63b341b7469ec05f72c80a519e7f8a425655
mrholek <lukasz@holeczek.pl>
2023-08-02 23:54:37 +0200

    docs: udpate theme

--- packages/docs/src/components/Toc.tsx
+++ packages/docs/src/components/Toc.tsx
@@ -34,7 +34,7 @@ const Toc: FC<TocProps> = ({ items }) => {
   }
 
   return (
-    <div className="docs-toc mt-4 mb-5 my-md-0 ps-xl-5 mb-lg-5 text-muted">
+    <div className="docs-toc mt-4 mb-5 my-md-0 ps-xl-5 mb-lg-5 text-body-secondary">
       <strong className="d-block h6 mb-2 pb-2 border-bottom">On this page</strong>
       <CNav component="nav" className="flex-column">
         <ul>{toc(items)}</ul>
