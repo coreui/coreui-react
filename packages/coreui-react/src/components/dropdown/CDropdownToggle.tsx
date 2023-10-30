1a49dda772a0ffbed9c1d256532cae618a46f998
mrholek <lukasz@holeczek.pl>
2023-10-30 15:41:13 +0100

    release: v5.0.0-beta.0

--- packages/coreui-react/src/components/dropdown/CDropdownToggle.tsx
+++ packages/coreui-react/src/components/dropdown/CDropdownToggle.tsx
@@ -21,7 +21,7 @@ export interface CDropdownToggleProps extends Omit<CButtonProps, 'type'> {
   /**
    * If a dropdown `variant` is set to `nav-item` then render the toggler as a link instead of a button.
    * 
-   * @since v5.0.0-alpha.3
+   * @since v5.0.0-beta.0
    */
   navLink?: boolean
   /**
