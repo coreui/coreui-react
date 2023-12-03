7b22139e0c91f91cde6bd37f9243d15b6aa7fbfd
Fidel <fidel.codes@gmail.com>
2023-12-03 21:15:03 +0100

    dinamically creates explorer links to collators and validators

--- src/_nav.js
+++ src/_nav.js
@@ -10,73 +10,159 @@ import {
 } from '@coreui/icons'
 import { CNavGroup, CNavItem, CNavTitle, CNavLink } from '@coreui/react'
 
-const _nav = [
-   {
-    component: CNavTitle,
-    name: "Account"
-  },
-  {
-    component: CNavTitle,
-    name: 'Current Deployment',
-  },
-  {
-    component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
-    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Coretime Credits',
-    to: '/coretime',
-    icon: <CIcon icon={cilWallet} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Runtime Upgrades',
-    to: '/runtime-upgrade',
-    icon: <CIcon icon={cilMemory} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavItem,
-    name: 'Explorer',
-    to: 'https://google.com',
-    icon: <CIcon icon={cilMagnifyingGlass} customClassName="nav-icon" />,
-  },
-  {
-    component: CNavLink,
-    name: 'Grafana',
-    href: 'https://google.com',
-    icon: <CIcon icon={cilMonitor} customClassName="nav-icon" />,
-  },
-  //  TODO -> There could be some logic here to show one or the other menu depending on if there's an active deployment
-  // {
-  //   component: CNavTitle,
-  //   name: 'New Deployment',
-  // },
-  // {
-  //   component: CNavGroup,
-  //   name: 'Configuration',
-  //   to: '/configure',
-  //   icon: <CIcon icon={cilCog} customClassName="nav-icon" />,
-  //   items: [
-  //     {
-  //       component: CNavItem,
-  //       name: 'Runtime',
-  //       to: '/configure/runtime',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Collators',
-  //       to: '/configure/collators',
-  //     },
-  //     {
-  //       component: CNavItem,
-  //       name: 'Coretime',
-  //       to: '/configure/coretime',
-  //     },
-  //   ],
-  // },
-]
+const generateNav = (networkInfo, networkStatus) => {
+  let relayChainValidators = [];
+  let paraCollators = [];
 
-export default _nav
+  if (networkStatus === 'OK') {
+    //this means that we have successfully launched the networks.
+    //objective is to generate now the rest of the nav array with the information of the collators and validators
+    relayChainValidators = networkInfo.relay.map(node => {
+      return {
+        component: CNavItem,
+        name: node.name,
+        to: `https://polkadot.js.org/apps/?rpc=${node.wsUri}#/explorer`
+      }
+    })
+    
+    paraCollators = networkInfo.paras[1].map(node => {
+      return {
+        component: CNavItem,
+        name: node.name,
+        to: `https://polkadot.js.org/apps/?rpc=${node.wsUri}#/explorer`
+      }
+    })
+  }
+
+  
+  const nav = [
+    {
+     component: CNavTitle,
+     name: "Account"
+   },
+   {
+     component: CNavTitle,
+     name: 'Current Deployment',
+   },
+   {
+     component: CNavItem,
+     name: 'Dashboard',
+     to: '/dashboard',
+     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   },
+   {
+     component: CNavItem,
+     name: 'Coretime Credits',
+     to: '/coretime',
+     icon: <CIcon icon={cilWallet} customClassName="nav-icon" />,
+   },
+   {
+     component: CNavItem,
+     name: 'Runtime Upgrades',
+     to: '/runtime-upgrade',
+     icon: <CIcon icon={cilMemory} customClassName="nav-icon" />,
+   },
+   {
+      component: CNavGroup,
+        name: 'Explorer',
+        icon: <CIcon icon={cilMagnifyingGlass} customClassName="nav-icon" />,
+        items: [
+          {
+            component: CNavGroup,
+            name: 'Relaychain',
+            items:relayChainValidators
+          },
+          {
+            component: CNavGroup,
+            name: 'Parachain',
+            items:paraCollators
+          }
+        ]
+   }
+  ]
+
+  return nav
+  
+}
+
+// const _nav = [
+//    {
+//     component: CNavTitle,
+//     name: "Account"
+//   },
+//   {
+//     component: CNavTitle,
+//     name: 'Current Deployment',
+//   },
+//   {
+//     component: CNavItem,
+//     name: 'Dashboard',
+//     to: '/dashboard',
+//     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+//   },
+//   {
+//     component: CNavItem,
+//     name: 'Coretime Credits',
+//     to: '/coretime',
+//     icon: <CIcon icon={cilWallet} customClassName="nav-icon" />,
+//   },
+//   {
+//     component: CNavItem,
+//     name: 'Runtime Upgrades',
+//     to: '/runtime-upgrade',
+//     icon: <CIcon icon={cilMemory} customClassName="nav-icon" />,
+//   },
+//     {
+//       component: CNavGroup,
+//       name: 'Explorer',
+//       icon: <CIcon icon={cilCog} customClassName="nav-icon" />,
+//       items: [
+//         {
+//           component: CNavGroup,
+//           name: 'Relay Chain',
+//           items: [
+//             {
+//               component: CNavItem,
+//               name: 'Validator - 01',
+//               to: '/'
+//             },
+//             {
+//               component: CNavItem,
+//               name: 'Validator - 02',
+//               to: '/'
+//             },
+//           ]
+//         },
+//         {
+//           component: CNavGroup,
+//           name: 'Parachain',
+//           items: [
+//             {
+//               component: CNavItem,
+//               name: 'Collator - 01',
+//               to: '/'
+//             },
+//             {
+//               component: CNavItem,
+//               name: 'Collator - 02',
+//               to: '/'
+//             },
+//           ]
+//         },
+//       ]
+//   },
+//   {
+//     component: CNavItem,
+//     name: 'Explorer',
+//     to: 'https://google.com',
+//     icon: <CIcon icon={cilMagnifyingGlass} customClassName="nav-icon" />,
+//   },
+//   {
+//     component: CNavLink,
+//     name: 'Grafana',
+//     href: 'https://google.com',
+//     icon: <CIcon icon={cilMonitor} customClassName="nav-icon" />,
+//   },
+// ]
+
+export default generateNav
