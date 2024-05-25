ba42bcbd5692f5c8298ebaf78dd82fde1d158c3d
mrholek <lukasz@holeczek.pl>
2024-05-25 20:33:25 +0200

    docs: improve sidebar navigation

--- packages/docs/src/components/SidebarNav.tsx
+++ packages/docs/src/components/SidebarNav.tsx
@@ -48,10 +48,10 @@ export const SidebarNav = ({ items }: SidebarNavProps) => {
   }
 
   const navGroup = (item: NavItem, index: number) => {
-    const { name, icon, ...rest } = item
+    const { name, icon, items } = item
     return (
-      <CNavGroup compact toggler={navLink(name, icon)} key={index} {...rest}>
-        {item.items?.map((item: NavItem, index: number) =>
+      <CNavGroup compact toggler={navLink(name, icon)} key={index}>
+        {items?.map((item: NavItem, index: number) =>
           item.items ? navGroup(item, index) : navItem(item, index),
         )}
       </CNavGroup>
