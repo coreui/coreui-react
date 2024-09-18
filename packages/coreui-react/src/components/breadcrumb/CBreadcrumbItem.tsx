b5782510e7004f5dd3379db63945ca5b961666a8
mrholek <lukasz@holeczek.pl>
2024-09-18 23:39:14 +0200

    feat(CBreadcrumb): allow to pass custom components as breadcrumb item

--- packages/coreui-react/src/components/breadcrumb/CBreadcrumbItem.tsx
+++ packages/coreui-react/src/components/breadcrumb/CBreadcrumbItem.tsx
@@ -1,14 +1,22 @@
-import React, { forwardRef, HTMLAttributes } from 'react'
+import React, { ElementType, forwardRef, HTMLAttributes } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 import { CLink } from '../link/CLink'
+
+import { PolymorphicRefForwardingComponent } from '../../helpers'
 
 export interface CBreadcrumbItemProps extends HTMLAttributes<HTMLLIElement> {
   /**
    * Toggle the active state for the component.
    */
   active?: boolean
+  /**
+   * Component used for the root node. Either a string to use a HTML element or a component.
+   *
+   * @since 5.4.0
+   */
+  as?: ElementType
   /**
    * A string of all className you want applied to the base component.
    */
@@ -19,26 +27,33 @@ export interface CBreadcrumbItemProps extends HTMLAttributes<HTMLLIElement> {
   href?: string
 }
 
-export const CBreadcrumbItem = forwardRef<HTMLLIElement, CBreadcrumbItemProps>(
-  ({ children, active, className, href, ...rest }, ref) => {
-    return (
-      <li
-        className={classNames(
-          'breadcrumb-item',
-          {
-            active: active,
-          },
-          className,
-        )}
-        {...(active && { 'aria-current': 'page' })}
-        {...rest}
-        ref={ref}
-      >
-        {href ? <CLink href={href}>{children}</CLink> : children}
-      </li>
-    )
-  },
-)
+export const CBreadcrumbItem: PolymorphicRefForwardingComponent<'li', CBreadcrumbItemProps> =
+  forwardRef<HTMLLIElement, CBreadcrumbItemProps>(
+    ({ children, active, as, className, href, ...rest }, ref) => {
+      return (
+        <li
+          className={classNames(
+            'breadcrumb-item',
+            {
+              active: active,
+            },
+            className,
+          )}
+          {...(active && { 'aria-current': 'page' })}
+          {...rest}
+          ref={ref}
+        >
+          {href ? (
+            <CLink as={as} href={href}>
+              {children}
+            </CLink>
+          ) : (
+            children
+          )}
+        </li>
+      )
+    },
+  )
 
 CBreadcrumbItem.propTypes = {
   active: PropTypes.bool,
